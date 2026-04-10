5d132ce7f708c1bf53a9594d0f60e8cc8cf7deaa
Witte's Workbench <201644942+witte-workbench@users.noreply.github.com>
2026-04-10 13:32:55 -0600

    Removed unnecessary comments

--- style.js
+++ style.js
@@ -6,6 +6,9 @@ for(let div of document.getElementsByClassName('stickyDiv')) {
     div.classList.add([
         "red", "blue", "orange", "green", "purple"
     ].random() + "Sticky");
+    let rand = Math.ceil(Math.random()*3);
+    div.style.setProperty('--mask', 'url("images/sticky_note' + rand + '.svg")');
+    div.style.transform = `translate(${Math.round((Math.random()-0.5)*15)}px, ${Math.round((Math.random()-0.5)*15)}px)`;
 }
 if (localStorage.getItem("font")=="accessible") {
     document.body.classList.add("typewritten");
